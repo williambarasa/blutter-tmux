9b21dc2470ecf22e46dcd01b01b2ec569c5e50b4
Abhi <allinoneallinone00@gmail.com>
2024-09-25 01:38:16 +0530

    improve version checking for different suffixes

--- dartvm_fetch_build.py
+++ dartvm_fetch_build.py
@@ -65,9 +65,11 @@ class DartLibInfo:
         
         if os.path.exists(os.path.join(SCRIPT_DIR, "bin")):
             file_name_version = []
+            suffixes = ["", "_no-analysis", "_ida-fcn", "_no-analysis_ida-fcn"]
             for file in os.listdir(os.path.join(SCRIPT_DIR, "bin")):
-                if file.startswith("blutter_dartvm") and file.endswith(f"{os_name}_{arch}"):
-                    file_name_version.append(file.split("_")[1].replace('dartvm', ''))
+                for suffix in suffixes:
+                    if file.startswith("blutter_dartvm") and file.endswith(f"{os_name}_{arch}{suffix}"):
+                        file_name_version.append(file.split("_")[1].replace('dartvm', ''))
 
             for key, versions in dart_versions.items():
                 if version in versions and any(v in versions for v in file_name_version):
